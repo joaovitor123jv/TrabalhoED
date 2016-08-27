33114dd593d2d298985d9865853a8846a396c913
João Vitor Antoniassi Segantin <jv.joaovitoras@gmail.com>
2016-08-27 12:45:09 -0300

    Segunda mudança

--- Inicio.c
+++ Inicio.c
@@ -2,6 +2,29 @@
 
 void main()
 {
-	printf("Testando 1 2 3 \n");
-	printf("Aprendendo a mexer com git pelo terminal !!! \n");
+	int num;
+	int i;
+	int variavel[];
+	int operacao;
+	printf("Digite o numero de operações \n");
+	scanf("%d", &num);
+	for(i = 0; i<num; i++)
+	{
+		printf("Digite a operação (1 = inserção, 2 = remoção)\n");
+		scanf("%d", &operacao);
+		printf("Digite o elemento a ser inserido/removido\n");
+		scanf("%d", &variavel[num]);
+		if(operacao == 1)
+		{
+			printf("Isso foi uma inserção\n");
+		}
+		else if(operacao == 2)
+		{
+			printf(" Isso foi uma remoção\n");
+		}
+		else
+		{
+			printf("Operação não suportada\n");
+		}
+	}
 }
