652ec4b3ffbffe9460cdeb848b50d4da3018fb0a
João Vitor Antoniassi Segantin <jv.joaovitoras@gmail.com>
2016-09-05 15:17:51 -0300

    Código atualizado, editado para uma melhor organização

---
+++ CódigoFuncionandoSemInterface/Sacola.c
@@ -0,0 +1,78 @@
+#include"Sacola.h"
+
+void insere_fila ( int valor, Sacola *sacola )
+{
+	sacola->elemento_fila[ sacola->final_fila++ ] = valor;
+} 
+
+int remove_fila ( Sacola* sacola ) 
+{
+	if ( sacola->comeco_fila == sacola->final_fila ) 
+	{
+		return -10000000;
+	}
+	return sacola->elemento_fila[ sacola->comeco_fila++ ];
+}
+
+void insere_pilha ( int valor, Sacola *sacola ){
+	sacola->elemento_pilha[ ++sacola->indice_pilha ] = valor;
+}
+
+int remove_pilha ( Sacola *sacola ){
+	if ( sacola->indice_pilha <= 0 ) {
+		return  -10000000;
+	}
+	return sacola->elemento_pilha[ sacola->indice_pilha-- ];
+}
+
+void insere_lista_prioridade ( int valor, Sacola *sacola ){
+	int indexador_auxiliar;
+	indexador_auxiliar = ++sacola->indice_lista_prioridade;
+	while ( indexador_auxiliar > 1 ) 
+	{
+		if ( sacola->elemento_lista_prioridade[ indexador_auxiliar/2 ] < valor ) 
+		{
+			sacola->elemento_lista_prioridade[ indexador_auxiliar ] = sacola->elemento_lista_prioridade[ indexador_auxiliar/2 ];
+			indexador_auxiliar=indexador_auxiliar/2;
+		} 
+		else
+		{
+			break;
+		}
+	}
+	sacola->elemento_lista_prioridade[ indexador_auxiliar ] = valor;
+}
+
+int remove_lista_prioridade ( Sacola *sacola )
+{
+	int temporario, atual, proximo;
+	if ( sacola->indice_lista_prioridade <= 0 ) 
+	{
+		return  -10000000;
+	}
+
+	temporario = sacola->elemento_lista_prioridade[ 1 ];
+	atual = 1;
+	proximo = 2;
+	sacola->indice_lista_prioridade--;
+	while ( proximo <= sacola->indice_lista_prioridade )
+	{
+		if ( proximo < sacola->indice_lista_prioridade && sacola->elemento_lista_prioridade[ proximo+1 ] > sacola->elemento_lista_prioridade[ proximo ] ) 
+		{
+			proximo++;
+		}
+		if( sacola->elemento_lista_prioridade[ proximo ] > sacola->elemento_lista_prioridade[ sacola->indice_lista_prioridade+1 ] ) 
+		{
+			sacola->elemento_lista_prioridade[ atual ] = sacola->elemento_lista_prioridade[ proximo ];
+			atual = proximo;
+			proximo = atual*2;
+		}
+		else
+		{
+			break;
+		}
+	}
+	sacola->elemento_lista_prioridade[ atual ] = sacola->elemento_lista_prioridade[ sacola->indice_lista_prioridade+1 ];
+	return temporario;
+}
+
