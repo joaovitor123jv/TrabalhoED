36fabfd7bd2983c0c2806bd111183ca408dad052
João Vitor Antoniassi Segantin <jv.joaovitoras@gmail.com>
2016-10-25 16:42:37 -0200

    Update TipoRetorno.java

--- src/Interface/TipoRetorno.java
+++ src/Interface/TipoRetorno.java
@@ -1,12 +1,12 @@
 public class TipoRetorno
 {
-	public static int OK = 0;
-	public static int FILA = 5;
-	public static int PILHA = 6;
-	public static int FILA_DE_PRIORIDADES = 7;
-	public static int IMPOSSIVEL = 8;
-	public static int INCERTO = 9;
-	public static int NAO_FOI_POSSIVEL_ABRIR_ARQUIVO = 10;
-	public static int ARQUIVO_NAO_EXISTE = 4;
+	public static final int OK = 0;
+	public static final int FILA = 5;
+	public static final int PILHA = 6;
+	public static final int FILA_DE_PRIORIDADES = 7;
+	public static final int IMPOSSIVEL = 8;
+	public static final int INCERTO = 9;
+	public static final int NAO_FOI_POSSIVEL_ABRIR_ARQUIVO = 10;
+	public static final int ARQUIVO_NAO_EXISTE = 4;
 
 }
